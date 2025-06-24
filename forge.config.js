f594f75aa349e0deca1e2631532fdf5644e0295e
fallenreaper <wllm.francis@gmail.com>
2025-06-24 15:17:21 -0400

    possible test deploy

---
+++ forge.config.js
@@ -0,0 +1,26 @@
+export default {
+  packagerConfig: {},
+  makers: [
+    {
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin", "linux", "windows"],
+      config: {
+        // identity: null, // Disable code signing for the maker
+        // the config can be an object
+      },
+    },
+  ],
+  publishers: [
+    {
+      name: "@electron-forge/publisher-github",
+      config: {
+        repository: {
+          owner: "EggyBoffer",
+          name: "Eve-Isk-Management",
+        },
+        prerelease: false,
+        draft: true,
+      },
+    },
+  ],
+};
