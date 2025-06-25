3d6335593b99556fabc50db1e5c316977bd11954
EggyBoffer <rbushell127@gmail.com>
2025-06-25 15:13:05 +0100

    Added automatic update checker. Added fillament selection to overlay. Updated version control. Version 0.0.2

--- electron/preload.js
+++ electron/preload.js
@@ -8,5 +8,15 @@ contextBridge.exposeInMainWorld('api', {
   getEntries: (category) => ipcRenderer.invoke('get-entries', category),
   openOverlay: () => ipcRenderer.send('open-overlay'),
   closeOverlay: () => ipcRenderer.send('close-overlay'),
-  deleteEntry: (category, id) => ipcRenderer.invoke('delete-entry', category, id), 
+  deleteEntry: (category, id) => ipcRenderer.invoke('delete-entry', category, id),
+  openOverlayWithCost: (cost) => ipcRenderer.send('open-overlay-with-cost', cost),
+});
+
+contextBridge.exposeInMainWorld('electron', {
+  ipcRenderer: {
+    on: (channel, callback) => ipcRenderer.on(channel, callback),
+    send: (channel, data) => ipcRenderer.send(channel, data),
+    removeAllListeners: (channel) => ipcRenderer.removeAllListeners(channel),
+     receiveFilamentSettings: (callback) => ipcRenderer.on('set-filament-settings', (_, settings) => callback(settings))
+  },
 });
